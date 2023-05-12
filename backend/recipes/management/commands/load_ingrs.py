367685774de46a69e00d9c29e3e094a973c82268
Anton Korshunov <042493@mail.ru>
2023-05-12 17:23:42 +0300

    +++++

--- backend/recipes/management/commands/load_ingrs.py
+++ backend/recipes/management/commands/load_ingrs.py
@@ -1,22 +1,25 @@
 import csv
+import os
 
 from django.conf import settings
-from django.core.management import BaseCommand
-
+from django.core.management.base import BaseCommand
 from recipes.models import Ingredient
 
 
-class Command(BaseCommand):
-    help = 'Загрузка из csv файла'
+def get_reader(file_name: str):
+    csv_path = os.path.join(settings.BASE_DIR, './data/', file_name)
+    csv_file = open(csv_path, 'r', encoding='utf-8')
+    return csv.reader(csv_file, delimiter=',')
 
-    def handle(self, *args, **kwargs):
-        data_path = settings.BASE_DIR
-        with open(
-            f'{data_path}/data/ingredients.csv',
-            'r',
-            encoding='utf-8'
-        ) as file:
-            reader = csv.DictReader(file)
-            Ingredient.objects.bulk_create(
-                Ingredient(**data) for data in reader)
-        self.stdout.write(self.style.SUCCESS('Все ингридиенты загружены!'))
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        csv_reader = get_reader('ingredients.csv')
+        next(csv_reader, None)
+        for row in csv_reader:
+            obj, created = Ingredient.objects.get_or_create(
+                name=row[0],
+                measurement_unit=row[1]
+            )
+        print('ingredients - OK')
