9bee414653e42cca38e9b05f0c19184a94284c9e
Anton Korshunov <042493@mail.ru>
2023-05-14 12:49:13 +0300

    Обновление по итогам ревью 2го этапа проверки диплома

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -4,12 +4,28 @@ from .models import (Favourite, Ingredient, IngredientAmount, Recipe,
                      ShoppingCart, Tag)
 
 
+
+class IngredientAmountAdmin(admin.StackedInline):
+    model = IngredientAmount
+    autocomplete_fields = ('ingredient',)
+
+
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = (
         'id', 'name', 'author', 'cooking_time', 'pub_date', 'in_favorites',)
     list_filter = ('author', 'name', 'pub_date', 'tags',)
+    inlines = (IngredientAmountAdmin,)
     empty_value_display = '-нет рецептов-'
+
+    @admin.display(description='Ингредиенты')
+    def get_ingredients(self, obj):
+        return '\n '.join([
+            f'{item["ingredient__name"]} - {item["amount"]}'
+            f' {item["ingredient__measurement_unit"]}.'
+            for item in obj.recipe.values(
+                'ingredient__name',
+                'amount', 'ingredient__measurement_unit')])
 
     @admin.display(description='В избранном')
     def in_favorites(self, obj):
@@ -20,13 +36,6 @@ class RecipeAdmin(admin.ModelAdmin):
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'color', 'slug',)
     empty_value_display = '-нет тагов-'
-
-
-@admin.register(Ingredient)
-class IngredientAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'measurement_unit',)
-    list_filter = ('name',)
-    empty_value_display = '-нет ингредиентов-'
 
 
 @admin.register(ShoppingCart)
